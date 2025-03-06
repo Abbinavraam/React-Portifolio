371170a0eb05c39b42615d294393dc7797c2cc24
Abbinavraam <147265826+Abbinavraam@users.noreply.github.com>
2025-03-06 22:52:44 +0530

    commit

---
+++ src/data/user.js
@@ -0,0 +1,80 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Raam",
+		name: " Abbinav Raam",
+		email: "abbinavraam9@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/Abbinavraam",
+		linkedin: "linkedin.com/in/abbinav-raam-95214627a",
+	},
+
+	homepage: {
+		title: "Full-stack web and mobile app developer.",
+		description:
+			"I am a frontend developer specializing in ReactJs, with experience in building scalable, secure, and reliable web applications using various frameworks and technologies. I thrive on solving complex problems and continuously expanding my skill set. Passionate about writing high-quality code that adheres to best practices and industry standards, I am always eager to take on new challenges and explore opportunities for growth.",
+	},
+
+	about: {
+		title: "I’m Abbinav Raam I live in Hyderabad, where I design the future.",
+		description:
+			"I've worked on a variety of projects over time and take pride in the progress I've made. Many of these projects are open-source, allowing others to explore, contribute, and collaborate. If you're interested, feel free to check out the code and share any suggestions for improvements or enhancements. I believe collaboration is a valuable way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I am passionate about expanding the limits of possibility and inspiring the next generation of innovators.",
+		description:
+			"A chronological compilation of my in-depth thoughts on programming, leadership, product design, and beyond.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			// link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			// link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			// link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			// link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			// link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
